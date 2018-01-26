14d18e4178616be05331578f72d5f14e40de01c2
Sean Fox <geekypghrunner@gmail.com>
2018-01-26 13:27:39 +0000

    Add styling to app

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -32,10 +32,10 @@ function drawGrid() {
 
 //Change cell color on click
 $("#container").on("click", ".column", function() {
-  if ($(this).css("background-color") === "rgba(0, 0, 0, 0)") {
+  if ($(this).css("background-color") === "rgb(255, 255, 255)") {
     $(this).css("background-color", colorPicker);
   } else {
-    $(this).css("background-color", "rgba(0, 0, 0, 0)");
+    $(this).css("background-color", "rgb(255, 255, 255)");
   }
 });
 
@@ -86,13 +86,11 @@ function clear(){
 }
 
 function reinstate(){
-  colorByNumbers(($('#pictureChoice').val()));
+colorByNumbers(($('#pictureChoice').val()));
   $('.column').each(function(){
     $(this).css("border", "1px solid");
   });
 }
 
-
 const smileyPicture = "1111111111111111111111111111111111111111111111111111111111111111111222221111111111111223333322111111111123333333332111111112333333333332111111123333333333321111112333223332233321111123332233322333211111233333333333332111112333333333333321111123323333333233211111123323333323321111111233322222333211111111233333333321111111111223333322111111111111122222111111111111111111111111111111111111111111111111";
-
 });
